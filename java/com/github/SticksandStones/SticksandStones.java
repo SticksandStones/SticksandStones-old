552a58255e292fde805b009e270d19939d65faa5
coolsquid <sankkeflink@gmail.com>
2014-12-15 21:43:47 +0100

    Start.

---
+++ java/com/github/SticksandStones/SticksandStones.java
@@ -0,0 +1,26 @@
+package com.github.SticksandStones;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.modid, name = Reference.modid, version = Reference.version)
+public class SticksandStones {
+	
+	@EventHandler
+	public static void PreInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public static void Init(FMLInitializationEvent event) {
+		
+	}
+	
+	@EventHandler
+	public static void PostInit(FMLPostInitializationEvent event) {
+		
+	}
+}
